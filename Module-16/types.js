8ba9c9ebfb650106df6811c6db3167df2df5d502
Mosharof <md5470415@gmail.com>
2024-09-22 10:40:02 +0600

    js variable type

---
+++ Module-16/types.js
@@ -0,0 +1,13 @@
+// String 
+var myName = 'Mosharof Hossain';
+console.log(typeof myName);
+
+
+// Boolean 
+var isRich = false;
+console.log(typeof isRich);
+
+// Number 
+var price = 45;
+console.log(typeof price);
+
---
+++ Module-16/variable.js
@@ -0,0 +1,11 @@
+// Declare Variable 
+
+var height = 5.1;
+var price = 35;
+var mony = 40;
+
+// redeclear a variable 
+var age = 23;
+age = 24;
+
+console.log(age);
